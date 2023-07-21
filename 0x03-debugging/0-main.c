5ceef031dd53e5d03c276c17eb9984816710b0ec
ghostthewildcat <elytewonders@gmail.com>
2023-07-20 17:20:48 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
-#include "main.h
+#include "main.h"
 
 /**
- * main-tests function that prints integer is positive or negative
+ * main - tests function that prints integer is positive or negative
  * Return: 0
  */
 
@@ -12,5 +12,5 @@ int main(void)
 	i = 0;
 	positive_or_negative(i);
 
-	return (0)
+	return (0);
 }
